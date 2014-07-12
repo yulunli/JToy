30452264067b0a559d323adcc6e22f65381f1e18
yulunli <yulun.li2@gmail.com>
2014-07-11 17:26:58 -0700

    Solved divisor problem

--- src/main/java/apps/Solution.java
+++ src/main/java/apps/Solution.java
@@ -1,60 +1,92 @@
 package apps;
+
 import java.util.*;
 
-//http://www.quora.com/What-is-the-algorithmic-approach-to-solve-hackerrank-problem-Substring-Diff
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num_cases = in.nextInt();
+        int[] cases = new int[num_cases];
+        int[] casesUpdate = new int[num_cases];
         for (int kase = 0; kase < num_cases; kase++) {
-            int L = in.nextInt();
-            String s1 = in.next();
-            String s2 = in.next();
-            System.out.println(getBest(s1, s2, L));
+            cases[kase] = in.nextInt();
         }
-    }
-
-    public static int getBest(String s1, String s2, int L) {
-        int best = 0;
-        for (int i = 0; i < s1.length() - L + 1; i++) {
-            best = Math.max(Math.max(best, getBest(s1, s2, L, 0, i)), getBest(s1, s2, L, i, 0));
+        System.arraycopy(cases, 0, casesUpdate, 0, cases.length);
+        int[] num_divs = new int[num_cases];
+        for (int i = 0; i < num_cases; i++) {
+            int tempN = cases[i];
+            int count_2 = 0;
+            while (tempN % 2 == 0) {
+                count_2 += 1;
+                tempN = tempN / 2;
+            }
+            casesUpdate[i] = tempN;
+            num_divs[i] = count_2;
         }
-        return best;
-    }
-
-    public static int getBest(String s1, String s2, int L, int a, int b) {
-        System.out.println("*** \n" + a + " " + b);
-        int N = s1.length();
-        int i = a;
-        int j = b;
-        int l = 0;
-        int bad = 0;
-        int best = 0;
-        while (true) {
-            if ((i + l >= N) || (j + l >= N)) {
-                best = Math.max(best, l);
-                break;
-            }
-            System.out.println(s1.substring(i + l, i + l + 1) + " " + s2.substring(j + l, j + l + 1));
-            System.out.println(i + " " + j + " " + l + " " + best);
-            if (!s1.substring(i + l, i + l + 1).equals(s2.substring(j + l, j + l + 1))) {
-                bad += 1;
-            }
-            if (bad > L) {
-                best = Math.max(best, l);
-                while (s1.substring(i, i + 1).equals(s2.substring(j, j + 1))) {
-                    i += 1;
-                    j += 1;
-                    l -= 1;
+        int max = 0;
+        for (int i = 0; i < num_cases; i++) {
+            if (num_divs[i] > 0) {
+                if (casesUpdate[i] > max) {
+                    max = casesUpdate[i];
                 }
-                bad -= 1;
-                i += 1;
-                j += 1;
-            } else {
-                l += 1;
             }
         }
-        System.out.println("best: " + best);
-        return best;
+        List<Integer> primes = primes((int) Math.pow(max, 0.5) + 1 + 1);
+//        System.out.println(primes);
+        for (int i = 0; i < num_cases; i++) {
+            if (num_divs[i] > 0) {
+                int tempN = casesUpdate[i];
+                for (int j = 1; j < primes.size(); j++) {
+                    int prime = primes.get(j);
+                    int temp = 1;
+                    while (tempN % prime == 0) {
+                        temp += 1;
+                        tempN = tempN / prime;
+                    }
+                    casesUpdate[i] = tempN;
+                    num_divs[i] = num_divs[i] * temp;
+                    if (prime > tempN) {
+                        break;
+                    }
+                }
+                if (tempN > 1) {
+                    num_divs[i] = num_divs[i] * 2;
+                }
+            }
+        }
+        for (int num_div : num_divs) {
+            System.out.println(num_div);
+        }
+    }
+
+    // n > 1
+    public static List<Integer> primes(int n) {
+        List<Integer> primes = new ArrayList<Integer>();
+        if (n < 3) {
+            return primes;
+        }
+        boolean[] sieve = new boolean[n];
+        Arrays.fill(sieve, true);
+        for (int i = 3; i < Math.pow(n, 0.5) + 1; i += 2) {
+            if (sieve[i]) {
+                for (int j = i * 2; j < n; j+= i) {
+                    sieve[j] = false;
+                }
+            }
+        }
+        primes.add(2);
+        for (int i = 3; i < sieve.length; i += 2) {
+            if (sieve[i]) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+
+    public static void pr(boolean[] a) {
+        for (boolean anA : a) {
+            System.out.print(anA + " ");
+        }
+        System.out.println();
     }
 }
--- src/test/java/apps/SolutionTest.java
+++ src/test/java/apps/SolutionTest.java
@@ -4,7 +4,6 @@ import junit.framework.TestCase;
 
 public class SolutionTest extends TestCase {
     public static void testSolution() {
-        System.out.println(Solution.getBest("abacba", "abccza", 1));
-//        assertEquals(solution.solution(), "Hello!");
+        Solution.primes(1000000000/2);
     }
 }
