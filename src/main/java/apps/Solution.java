25406a1dd31251ff5e53685d8ede85e26de214d9
yulunli <yulun.li2@gmail.com>
2014-07-11 22:15:30 -0700

    solved select probability

--- src/main/java/apps/Solution.java
+++ src/main/java/apps/Solution.java
@@ -1,90 +1,113 @@
 package apps;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num_cases = in.nextInt();
-        int[] cases = new int[num_cases];
-        int[] casesUpdate = new int[num_cases];
         for (int kase = 0; kase < num_cases; kase++) {
-            cases[kase] = in.nextInt();
-        }
-        System.arraycopy(cases, 0, casesUpdate, 0, cases.length);
-        int[] num_divs = new int[num_cases];
-        for (int i = 0; i < num_cases; i++) {
-            int tempN = cases[i];
-            int count_2 = 0;
-            while (tempN % 2 == 0) {
-                count_2 += 1;
-                tempN = tempN / 2;
-            }
-            casesUpdate[i] = tempN;
-            num_divs[i] = count_2;
-        }
-        int max = 0;
-        for (int i = 0; i < num_cases; i++) {
-            if (num_divs[i] > 0) {
-                if (casesUpdate[i] > max) {
-                    max = casesUpdate[i];
-                }
-            }
-        }
-        List<Integer> primes = primes((int) Math.pow(max, 0.5) + 1 + 1);
-//        System.out.println(primes);
-        for (int i = 0; i < num_cases; i++) {
-            if (num_divs[i] > 0) {
-                int tempN = casesUpdate[i];
-                for (int j = 1; j < primes.size(); j++) {
-                    int prime = primes.get(j);
-                    int temp = 1;
-                    while (tempN % prime == 0) {
-                        temp += 1;
-                        tempN = tempN / prime;
-                    }
-                    casesUpdate[i] = tempN;
-                    num_divs[i] = num_divs[i] * temp;
-                    if (prime > tempN) {
-                        break;
-                    }
-                }
-                if (tempN > 1) {
-                    num_divs[i] = num_divs[i] * 2;
-                }
-            }
-        }
-        for (int num_div : num_divs) {
-            System.out.println(num_div);
+            int len = in.nextInt();
+            int K = in.nextInt();
+//            System.out.println(bf(in.next(), K));
+            System.out.println(printProb(in.next(), K));
         }
     }
 
-    // n > 1
-    public static List<Integer> primes(int n) {
-        List<Integer> primes = new ArrayList<Integer>();
-        if (n < 3) {
-            return primes;
+    public static String printProb(String in, int K) {
+        BigInteger count = BigInteger.ZERO;
+        int len = in.length();
+        int[] ns = new int[len];
+        for (int i = 0; i < len; i++) {
+            ns[i] = Integer.parseInt(String.valueOf(in.charAt(i)));
         }
-        boolean[] sieve = new boolean[n];
-        Arrays.fill(sieve, true);
-        for (int i = 3; i < Math.pow(n, 0.5) + 1; i += 2) {
-            if (sieve[i]) {
-                for (int j = i * 2; j < n; j+= i) {
-                    sieve[j] = false;
+        int cache = 0; // what's in the first slice
+        for (int i = 0; i < K; i++) {
+            cache += ns[i];
+        }
+        if (K == len) {
+            count = new BigInteger(Integer.valueOf(cache).toString()).multiply(new BigInteger(Integer.valueOf(cache).toString()));
+        } else {
+            cache += ns[K];
+            for (int i = 0; i < len - K; i++) {
+                // If reaching end
+                if (i + K == len - 1) {
+                    int tempCount = 0;
+                    for (int j = i; j < len; j++) {
+                        if (ns[j]  == 1) {
+                            tempCount += 1;
+                        }
+                    }
+                    count = count.add(arrange2(tempCount));
+                } else {
+                    if (ns[i] == 1) {
+                        if (cache > 1) {
+                            count = count.add(new BigInteger(Integer.valueOf(2 * (cache - 1)).toString()));
+                        }
+
+                    }
+                    // shift slice
+                    cache = cache - ns[i] + ns[i + K + 1];
+                }
+            }
+            for (int n : ns) {
+                count = count.add(new BigInteger(Integer.valueOf(n).toString()));
+            }
+        }
+        BigInteger base = BigDecimal.valueOf((Math.pow(len, 2))).toBigInteger();
+        BigInteger gcd = gcd(base, count);
+        return count.divide(gcd) + "/" + base.divide(gcd);
+    }
+
+    public static String bf(String in, int K) {
+        BigInteger count = BigInteger.ZERO;
+        int len = in.length();
+        int[] ns = new int[len];
+        for (int i = 0; i < len; i++) {
+            ns[i] = Integer.parseInt(String.valueOf(in.charAt(i)));
+        }
+        List<Integer> s = new ArrayList<Integer>();
+        for (int i = 0; i < len; i++) {
+            if (ns[i] == 1) {
+                s.add(i);
+            }
+        }
+        for (int n : s) {
+            for (int m: s) {
+                if (Math.abs(n - m) <= K) {
+                    count = count.add(BigInteger.ONE);
                 }
             }
         }
-        primes.add(2);
-        for (int i = 3; i < sieve.length; i += 2) {
-            if (sieve[i]) {
-                primes.add(i);
-            }
+        if (count.equals(BigInteger.ZERO)) {
+            return "0/1";
         }
-        return primes;
+        BigInteger base = BigDecimal.valueOf((Math.pow(len, 2))).toBigInteger();
+        BigInteger gcd = gcd(base, count);
+        return count.divide(gcd) + "/" + base.divide(gcd);
     }
 
-    public static void pr(boolean[] a) {
-        for (boolean anA : a) {
+    public static BigInteger gcd(BigInteger a, BigInteger b) {
+        while (!b.equals(BigInteger.ZERO)) {
+            BigInteger c = new BigInteger(b.toString());
+            b = a.mod(b);
+            a = new BigInteger(c.toString());
+        }
+        return a;
+    }
+
+    public static BigInteger arrange2(int n) {
+        if (n < 2) {
+            return BigInteger.ZERO;
+        } else {
+            return new BigInteger(Integer.valueOf(n).toString()).multiply(new BigInteger(Integer.valueOf(n).toString()).subtract(BigInteger.ONE));
+        }
+    }
+
+    public static void pr(int[] a) {
+        for (int anA : a) {
             System.out.print(anA + " ");
         }
         System.out.println();
--- src/test/java/apps/SolutionTest.java
+++ src/test/java/apps/SolutionTest.java
@@ -2,8 +2,11 @@ package apps;
 
 import junit.framework.TestCase;
 
+import java.math.BigInteger;
+
 public class SolutionTest extends TestCase {
     public static void testSolution() {
-        Solution.primes(1000000000/2);
+        System.out.println(Solution.gcd(new BigInteger(String.valueOf(17)), new BigInteger(String.valueOf(1))));
+//        Solution.primes(1000000000/2);
     }
 }
